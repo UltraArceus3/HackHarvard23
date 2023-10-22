f9e5648b3133213906ef19b0ffc8715c8fd7538a
Rany Kamel <rany@ranykamel.dev>
2023-10-22 06:38:19 -0400

    Changes to website, load for summarization transcript

--- website/frontend/static/js/scripts.js
+++ website/frontend/static/js/scripts.js
@@ -3,14 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
     var videoLinks = document.querySelectorAll('.video-link');
     var videoPlayer = document.querySelector("#player");
 
+    var transcript = document.querySelector(".transcription");
+
     videoLinks.forEach(function(link) {
         link.addEventListener('click', function(e) {
 
             e.preventDefault();
             var videoUrl = this.getAttribute('data_video');
+            var summary = this.getAttribute('transcript');
 
             videoPlayer.querySelector('source').setAttribute('src', "/static/" + videoUrl);
             
+            transcript.querySelector('p').innerHTML = summary;
+
             videoPlayer.load();
         });
     });
